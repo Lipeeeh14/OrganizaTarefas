9fd448dab8de789310709806381902fc39ce80db
Luiz Felipe <gilberto.barbosa@7comm.com.br>
2025-04-08 21:27:29 -0300

    FEAT: Implementa validação nos formulários de login e cadastro

--- Frontend/OrganizaTarefas/src/app/login/login.component.ts
+++ Frontend/OrganizaTarefas/src/app/login/login.component.ts
@@ -1,18 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  loginForm: FormGroup;
+
+  constructor(private router: Router) {
+    this.loginForm = new FormBuilder().group({
+      email: ['', Validators.required, Validators.email],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,20}$/)]]
+    });
+  }
 
   login(): void {
-    this.router.navigate(['/tarefas']);
+    if (this.loginForm.valid) {
+      console.log('User logged in:', this.loginForm.value);
+      this.router.navigate(['/tarefas']);
+    } else {
+      console.log('Form is invalid');
+    }    
   }
 
   register(): void {
--- Frontend/OrganizaTarefas/src/app/register/register.component.ts
+++ Frontend/OrganizaTarefas/src/app/register/register.component.ts
@@ -1,18 +1,36 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-  constructor(private router: Router) {}
+  registerForm: FormGroup;
+
+  constructor(private router: Router) {
+    this.registerForm = new FormBuilder().group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,20}$/)]]
+    });
+  }
+
+  register(): void {
+    if (this.registerForm.valid) {
+      console.log('User registered:', this.registerForm.value);
+      this.router.navigate(['/login']);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
   goToLogin(): void {
-    console.log('Login clicked!');
     this.router.navigate(['/login']);
   }
 }
